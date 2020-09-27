7300448ae0d0499ec7a91018b2ac3afd43235e0f
fede-vaccaro <federico.vaccaro@live.it>
2020-09-27 19:16:09 +0200

    Updated test.py

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def test(device, dqn, preprocess, game_params, draw_gif=True):
     with torch.no_grad():
         for i in range(max_steps):
             if draw_gif:
-                state_rgb = game.get_state().resize((400, 400), Image.NEAREST)
+                state_rgb = game.get_state(upscale=True).resize((400, 400), Image.NEAREST)
                 states.append(state_rgb)
 
             if game.is_terminal:
@@ -24,7 +24,7 @@ def test(device, dqn, preprocess, game_params, draw_gif=True):
                     print("Agent won in {} steps!".format(i))
                 break
 
-            state = game.get_state()
+            state = game.get_state(upscale=False)
 
             x = preprocess(state).to(device).unsqueeze(0)
             if random.uniform(0.0, 1.0) < 0.1:
@@ -52,14 +52,13 @@ if __name__ == '__main__':
         'n_holes': 16
     }
 
-    dqn = DQN(input_dim=game_dim**2*2, use_batch_norm=True)
+    dqn = MlpDQN(input_dim=game_dim**2*3, use_batch_norm=True)
     weights = torch.load(model_name)
     dqn.load_state_dict(weights)
     dqn.to(device)
 
     preprocess = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        torch.Tensor
     ])
     rewards = []
     for i in tqdm(range(1000)):
